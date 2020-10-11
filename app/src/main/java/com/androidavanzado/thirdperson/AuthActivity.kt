7adcacfae16158aa337435c59c5ee9bf5b3e044a
Edgar López Moreno <edlopmor@hotmail.es>
2020-10-11 19:13:02 +0200

    SE ha completado la parte de autentificacion desde GOOGlE  .

--- app/src/main/java/com/androidavanzado/thirdperson/AuthActivity.kt
+++ app/src/main/java/com/androidavanzado/thirdperson/AuthActivity.kt
@@ -1,24 +1,36 @@
 package com.androidavanzado.thirdperson
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_auth.*
 
 class AuthActivity : AppCompatActivity() {
     private val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
     private val bundle = android.os.Bundle()
 
+    private val GOOGLE_SIG_IN = 100
+
     enum class ProviderType {
-        BASIC
+        BASIC,
+        GOOGLE
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Thread.sleep(3000)//QUITAR ESTO CUANDO FUNCIONE
+        Thread.sleep(1500)//QUITAR ESTO CUANDO FUNCIONE
         setTheme(R.style.AppTheme)
 
 
@@ -33,6 +45,12 @@ class AuthActivity : AppCompatActivity() {
 
         //Setup
         Setup()
+        ConprobarUsuarioSession()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        authLayout.visibility = View.VISIBLE
     }
 
     private fun Setup() {
@@ -42,20 +60,8 @@ class AuthActivity : AppCompatActivity() {
         }
         buttonAcceder.setOnClickListener {
             if (editTextEmail.text.isNotEmpty() && editTextTextPassword.text.isNotEmpty()) {
-                FirebaseAuth.getInstance()
-                    .signInWithEmailAndPassword(
-                        editTextEmail.text.toString(),
-                        editTextTextPassword.text.toString()
-                    ).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            Toast.makeText(this, "Acceso exitoso", Toast.LENGTH_LONG).show()
-                            val intentToHome: Intent = Intent(this, HomeActivity::class.java)
-                            startActivity(intentToHome)
+                Acceder()
 
-                        } else {
-                            ShowAlert()
-                        }
-                    }
             } else {
                 if (editTextEmail.text.isEmpty())
                     Toast.makeText(this, "Rellene el campo de email", Toast.LENGTH_LONG).show()
@@ -67,8 +73,55 @@ class AuthActivity : AppCompatActivity() {
         textViewLostPassword.setOnClickListener {
             showLostPasswordActivity()
         }
+        buttonGoogle.setOnClickListener {
+            //Configuracion de acceso con cuenta google.
+            val googleAuthConfiguracion =
+                GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build()
+            val googleCliente = GoogleSignIn.getClient(this, googleAuthConfiguracion)
+            googleCliente.signOut()
+
+            startActivityForResult(googleCliente.signInIntent, GOOGLE_SIG_IN)
+        }
     }
 
+    /**
+     * Funcion que realiza la autentificacion a traves de los campos de texto mediante el usuario y la contraseña.
+     */
+    private fun Acceder() {
+        FirebaseAuth.getInstance()
+            .signInWithEmailAndPassword(
+                editTextEmail.text.toString(), editTextTextPassword.text.toString()
+            ).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Acceso exitoso", Toast.LENGTH_LONG).show()
+                    showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+
+                } else {
+                    ShowAlert()
+                }
+
+            }
+    }
+
+    /**
+     * Funcion que navega hasta la pantalla de usuario y envia los datos.
+     *
+     */
+    private fun showHome(email: String, provider: ProviderType) {
+
+        val homeIntent: Intent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
+
+    /**
+     * Metodo que muestra un aviso de error.
+     */
     private fun ShowAlert() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
@@ -78,17 +131,66 @@ class AuthActivity : AppCompatActivity() {
         dialog.show()
     }
 
+    /**
+     * Metodo para ir a la actividdad de registro.
+     */
     private fun showRegisterActivity() {
-        val homeIntent: Intent = Intent(this, RegisterActivity::class.java).apply {
+        val registerActivity: Intent = Intent(this, RegisterActivity::class.java).apply {
         }
-        startActivity(homeIntent)
+        startActivity(registerActivity)
 
     }
 
+    /**
+     * Metodo para ir a la actividad de recuperacion de contraseña .
+     */
     private fun showLostPasswordActivity() {
         val lostPasswordIntent: Intent = Intent(this, RetrievePasswordActivity::class.java).apply {
         }
         startActivity(lostPasswordIntent)
 
     }
+
+    /**
+     * Funcion que ve si tenemos algun usuario saltandose la primera pantalla en caso de que el usuario se haya registrado anteriormente.
+     */
+    private fun ConprobarUsuarioSession() {
+        val preferencias: SharedPreferences? =
+            getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email: String? = preferencias?.getString("email", null)
+        val provider: String? = preferencias?.getString("provider", null)
+
+        if (email != null && provider != null) {
+            authLayout.visibility = View.INVISIBLE
+            showHome(email, ProviderType.valueOf(provider))
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_SIG_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    val credencial = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credencial)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                showHome(it.result?.user?.email ?: "", ProviderType.GOOGLE)
+                            } else {
+                                ShowAlert()
+                            }
+                        }
+                }
+            } catch (e: ApiException) {
+                ShowAlert()
+            }
+
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/androidavanzado/thirdperson/HomeActivity.kt
+++ app/src/main/java/com/androidavanzado/thirdperson/HomeActivity.kt
@@ -1,11 +1,62 @@
 package com.androidavanzado.thirdperson
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
+
+enum class ProviderType {
+    BASIC,
+    GOOGLE
+}
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //Setup
+        val bundle: Bundle? = intent.extras
+        val email: String? = bundle?.getString("email")
+        val provider: String? = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+
+        //Guardar los datos de los usuarios autentificados en el dispositivo, de esta forma no es necesario que cada vez que entren a la aplicacion se autentifiquen.
+
+        val preferencias: SharedPreferences.Editor? =
+            getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        preferencias?.putString("email", email)
+        preferencias?.putString("provider", provider)
+        preferencias?.apply()
+
+        btnCerrarSesion.setOnClickListener {
+            cerrarSesion()
+        }
+
+    }
+
+    private fun setup(email: String, provider: String) {
+        title = "Inicio"
+        textViewEmail.text = email
+        textViewProvider.text = provider
+
+        //Programar el botton de log-out.
+
+
+    }
+
+    /**
+     * Boton que cierra la sesion.
+     *
+     */
+    private fun cerrarSesion() {
+        val preferencias: SharedPreferences.Editor? =
+            getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        preferencias?.clear()
+        preferencias?.apply()
+        FirebaseAuth.getInstance().signOut()
+        onBackPressed()
     }
 }
